d8b45cc1a57f9a8991f216ca89063dbbdb200a64
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2022-12-25 14:08:20 +0530

    Contact page update

--- src/app/home/page/contact/contact.component.ts
+++ src/app/home/page/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 import {MatSnackBar} from '@angular/material/snack-bar';
 import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
@@ -9,28 +10,54 @@ import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-  email  = '';
-  subject = '';
-  message = '';
+
   form : any;
 
-  constructor(private _snackBar: MatSnackBar) { }
+  constructor(private _snackBar: MatSnackBar,private formBuilder : FormBuilder) {
+    this.form = this.formBuilder.group({
+      email : ['',Validators.compose([
+        Validators.required,
+        Validators.email,
+        Validators.pattern(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/),
+        Validators.maxLength(320),
+        Validators.minLength(4)
+      ])],
+      myemail : ['brucearmstrongofficial@gmail.com'],
+      subject : ['',Validators.compose([
+        Validators.required,
+        Validators.maxLength(60),
+        Validators.minLength(10)
+      ])],
+      message : ['',Validators.compose([
+        Validators.required,
+        Validators.maxLength(500),,
+        Validators.minLength(10)
+      ])],
+    })
+   }
 
   ngOnInit(): void {
   }
 
+  get f(){
+    return this.form?.controls;
+  }
+
 send(event : any){
-  console.log(event.target)
-emailjs.sendForm('service_jgojg9h', 'template_7gg8w9e', event.target, 'user_MPAJU2uzehVdsNbuLRx70')
-.then((result: EmailJSResponseStatus) => {
-  this.openSnackBar(result.text);
-}, (error : any) => {
-  this.openSnackBar(error.text);
-});
+if(this.f.valid){
+  emailjs.sendForm('service_jgojg9h', 'template_7gg8w9e', event.target, 'user_MPAJU2uzehVdsNbuLRx70')
+  .then((result: EmailJSResponseStatus) => {
+    if(result?.status === 200){
+      this.openSnackBar('Successfully Sent');
+    }
+  }, (error : any) => {
+    this.openSnackBar(error.text);
+  });
+}
 }
 
 openSnackBar(message :  any) {
-  this._snackBar.openFromComponent(message, {
+  this._snackBar.open(message,'Close',{
     duration: 5000,
   });
 }
--- src/app/home/page/contact/contact.module.ts
+++ src/app/home/page/contact/contact.module.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact.component';
 
@@ -18,11 +20,13 @@ const routes: Routes = [{
     ContactComponent,
   ],
   imports: [
-    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
     MatSnackBarModule,
+    MatTooltipModule,
     RouterModule.forChild(routes),
   ],
   exports: [],
