8443055ad17e158be1a1274d387cf7e22991797e
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-09-28 11:51:49 +0530

    Sending Email, work details updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'mysite';
+  constructor() {
+    if (environment.production) {
+      console.warn(`🚨 Console output is disabled on production!`);
+      console.log = function (): void {};
+      console.debug = function (): void {};
+      console.warn = function (): void {};
+      console.info = function (): void {};
+    }
+  }
 }
